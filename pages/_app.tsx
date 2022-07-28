a54207cbfb5bf13bbde13a07f214b87c09f56b05
ALI11IB <ali.la.ibrahem@gmail.com>
2022-07-28 11:15:36 +0300

    add mui theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,22 @@ import { Layout } from "@components/common";
 
 import { AppProps } from "next/app";
 import { FC } from "react";
+
+import { ThemeProvider, createTheme } from "@mui/material";
 // interface Props {
 //   children: React.ReactElement;
 // }
-
+const theme = createTheme();
 function MyApp({
   Component,
   pageProps,
 }: AppProps & { Component: { Layout: FC } }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
 
